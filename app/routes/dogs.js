31861082cf58fcf2fbc1b5e51f0b4afa09c5ec95
MattLong34 <mdlong34@gmail.com>
2020-11-03 10:15:17 -0700

    add dog instances

--- app/routes/dogs.js
+++ app/routes/dogs.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class DogsRoute extends Route {
+    model(){
+        return [{
+            id: 1,
+            name: "simba",
+        },
+        {
+            id: 2,
+            name: "nala",
+        }]
+    }
 }
